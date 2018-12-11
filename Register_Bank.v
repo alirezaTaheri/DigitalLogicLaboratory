c352655c09d38f85db6f0dc8da0f80111fae34ce
Alireza Taheri <alireza.taheri210@gmail.com>
2018-12-11 09:08:50 +0330

    Register Bank Indented

--- Register_Bank.v
+++ Register_Bank.v
@@ -1,21 +1,21 @@
-module RegisterBank(a, b, c, write, clk, aData, bData, cData);
-reg[63:0] registers [31:0];
-input [4:0] a,b,c;
-input write, clk;
-input  [63:0] cData;
-output reg [63:0] aData, bData;
+  module RegisterBank(a, b, c, write, clk, aData, bData, cData);
+   reg[63:0] registers [31:0];
+   input [4:0] a,b,c;
+   input write, clk;
+   input  [63:0] cData;
+   output reg [63:0] aData, bData;
 
-always @(clk)
-begin 
-aData = registers[a];
-bData = registers[b];
-end
+   always @(clk)
+   begin 
+   aData = registers[a];
+   bData = registers[b];
+   end
 
-always @(clk, write)
-begin
-if (write)
-begin
-registers[c] = cData;
-end
-end
-endmodule
+   always @(clk, write)
+   begin
+   if (write)
+   begin
+   registers[c] = cData;
+   end
+   end
+   endmodule
