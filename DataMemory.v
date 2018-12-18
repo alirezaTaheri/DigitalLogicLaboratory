75e5e22d38c4170efe0a976773a07899c56ee108
Alireza Taheri <alireza.taheri210@gmail.com>
2018-12-18 10:33:32 +0330

    Some Changes

--- DataMemory.v
+++ DataMemory.v
@@ -2,14 +2,18 @@ module DataMemory(clock, write, read, readData, writeData, address);
 input clock, read, write;
 input [63:0] address, writeData;
 output [63:0] readData;
-reg [63:0] data[0:1000];
-
+reg [63:0] data[0:4];
 always @(posedge clock)
 begin
+data[0] = 64'd5;
+data[1] = 64'd5;
+data[2] = 64'd5;
 data[3] = 64'd5;
+data[4] = 64'd5;
 if (write == 1'b1)
 data[address] = writeData;
 end
 
 assign readData = (read == 1'b1 ? data[address] : readData);
+
 endmodule 
--- InstructionMemory.v
+++ InstructionMemory.v
@@ -3,9 +3,15 @@ module InstructionMemory(clock, readAddress, instruction);
 input clock;
 input [63:0] readAddress;
 output reg [31:0] instruction;
-reg [31:0] instructions [0:1000];
+reg [31:0] instructions [0:3];
 always @(posedge clock)
 begin
+
+instructions[0] = 32'h8B1F03E5;
+instructions[1] = 32'hF84000A4;
+instructions[2] = 32'h8B040086;
+instructions[3] = 32'hF80010A6;
+
 instruction = instructions[readAddress];
 end
 endmodule 
